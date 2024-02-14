53dd7737a703915648eb9c7e002bb06cdb993737
Tharindu Dimantha <walahanduwathilakarathna@gmail.com>
2024-02-14 11:46:45 +0530

    create project

---
+++ FirstMVCAPP/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Diagnostics;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+     
+     public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+
+
+    }
+}
